62c5920522643ce1aa11c993cb9d63d59ef8162e
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2015-02-02 03:29:29 +0000

    fixed static

--- app/src/main/java/team3m/dulwichoutdoorgallery/Core.java
+++ app/src/main/java/team3m/dulwichoutdoorgallery/Core.java
@@ -3,17 +3,15 @@ package team3m.dulwichoutdoorgallery;
 import java.util.ArrayList;
 
 public class Core {
-    public ArrayList<Art> Gallery = new ArrayList<Art>(){
+    public static ArrayList<Art> Gallery = new ArrayList<Art>(){
         {
             add(new Art("name" , "description" , "Author" , null , new ArrayList<String>()));
         }
     };
 
-    public ArrayList<Badge> badges = new ArrayList<Badge>(){
+    public static ArrayList<Badge> badges = new ArrayList<Badge>(){
         {
             add(new Badge("title" , "description"));
         }
     };
-
-
 }
