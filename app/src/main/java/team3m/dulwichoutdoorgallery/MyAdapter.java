ec7f66a169c29d7bd0a2eead86923da6583cf120
maria-v25 <maria.vrabie@kcl.ac.uk>
2015-02-12 16:42:29 +0000

    added author to the list

--- app/src/main/java/team3m/dulwichoutdoorgallery/MyAdapter.java
+++ app/src/main/java/team3m/dulwichoutdoorgallery/MyAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -27,6 +28,17 @@ public class MyAdapter extends ArrayAdapter<Art>{
         }
 
         Art a = getItem(position);
+        TextView textName= (TextView) itemView.findViewById(R.id.textView);
+        textName.setText(a.getName());
+
+        TextView textAuthor= (TextView)itemView.findViewById(R.id.textAuthor);
+        textAuthor.setText(a.getAuthor());
+
+        TextView textDescription= (TextView) itemView.findViewById(R.id.textDescription);
+        textDescription.setText(a.getDescription());
+
             return itemView;
+
+
     }
 }
