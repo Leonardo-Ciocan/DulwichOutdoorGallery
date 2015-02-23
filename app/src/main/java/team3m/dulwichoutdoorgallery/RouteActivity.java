b9ff4910b97b2d8c07642394edce86421dbe69fc
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2015-02-23 00:52:18 +0000

    Routing features + animations + sharing

--- app/src/main/java/team3m/dulwichoutdoorgallery/RouteActivity.java
+++ app/src/main/java/team3m/dulwichoutdoorgallery/RouteActivity.java
@@ -6,17 +6,26 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -80,6 +89,7 @@ public class RouteActivity extends ActionBarActivity {
         int lastVisited = 0;
         GoogleMap map;
         LatLng last;
+        boolean collapsed;
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -135,6 +145,86 @@ public class RouteActivity extends ActionBarActivity {
 
                 }
             });
+
+            Button navigateBtn = (Button) rootView.findViewById(R.id.navigateBtn);
+            navigateBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    navigate();
+                }
+            });
+
+            Button shareBtn = (Button) rootView.findViewById(R.id.shareBtn);
+            shareBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    share();
+                }
+            });
+
+
+            final ImageView artCardImage = (ImageView)rootView.findViewById(R.id.artCardImage);
+            final ImageView smallImage = (ImageView)rootView.findViewById(R.id.smallImage);
+
+
+            final CardView navigationCard = (CardView) rootView.findViewById(R.id.navigationCard);
+            final CardView currentArt = (CardView) rootView.findViewById(R.id.currentArt);
+
+            collapsed = true;
+
+            currentArt.setOnTouchListener(new View.OnTouchListener() {
+
+
+                @Override
+                public boolean onTouch(final View v, MotionEvent event) {
+                    Animation a = new Animation() {
+                        @Override
+                        protected void applyTransformation(float interpolatedTime, Transformation t) {
+
+                            Log.v("appi", interpolatedTime + " " + collapsed);
+                            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) v.getLayoutParams();
+                            params.bottomMargin = (int) ((-80 + (int) (70 * ((collapsed) ? interpolatedTime : 1 - interpolatedTime))) * getActivity().getResources().getDisplayMetrics().density);
+                            v.setLayoutParams(params);
+
+                            FrameLayout.LayoutParams params2 = (FrameLayout.LayoutParams) navigationCard.getLayoutParams();
+                            params2.bottomMargin = (int) (-25 - (int) (190 * ((collapsed) ? interpolatedTime : 1 - interpolatedTime)) * getActivity().getResources().getDisplayMetrics().density);
+                            navigationCard.setLayoutParams(params2);
+
+                           /* FrameLayout.LayoutParams params3 =(FrameLayout.LayoutParams) smallImage.getLayoutParams();
+                            params3.topMargin = (int)(5 + (int)(30 * ((collapsed)?interpolatedTime:1-interpolatedTime))* getActivity().getResources().getDisplayMetrics().density);
+                            smallImage.setLayoutParams(params3);
+*/
+
+
+
+                            artCardImage.setScaleY(2 - ((collapsed) ? interpolatedTime : 1 - interpolatedTime));
+                            artCardImage.setScaleX(2 - ((collapsed) ? interpolatedTime : 1 - interpolatedTime));
+
+                            navigationCard.setScaleY(1 + ((collapsed) ? interpolatedTime : 1 - interpolatedTime));
+                            navigationCard.setScaleX(1 + ((collapsed) ? interpolatedTime : 1 - interpolatedTime));
+                            navigationCard.setAlpha(1- ((collapsed) ? interpolatedTime : 1 - interpolatedTime));
+
+                            artCardImage.setAlpha( ((collapsed) ? interpolatedTime : 1 - interpolatedTime));
+
+                            if (interpolatedTime == 1) {
+                                collapsed = !collapsed;
+                                this.cancel();
+                            }
+                        }
+
+                        @Override
+                        public boolean willChangeBounds() {
+                            return true;
+                        }
+                    };
+
+                    a.setInterpolator(new AccelerateDecelerateInterpolator());
+                    a.setDuration(655);
+                    v.startAnimation(a);
+                    return false;
+                }
+            });
+
             return rootView;
         }
 
@@ -160,7 +250,7 @@ public class RouteActivity extends ActionBarActivity {
             ArrayList<Art> Gallery = Core.getGallery();
             for(int i=0; i< Core.getGallery().size(); i++){
                 LatLng point = new LatLng(Gallery.get(i).getLatitude(), Gallery.get(i).getLongitude());
-                map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+                map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(i <=1 ? R.drawable.marker_off : R.drawable.marker))
                         .title(Gallery.get(i).getName())
                         .snippet(Gallery.get(i).getDescription())
                         .position(point));
@@ -186,8 +276,16 @@ public class RouteActivity extends ActionBarActivity {
         void share(){
             Intent intent2 = new Intent(); intent2.setAction(Intent.ACTION_SEND);
             intent2.setType("text/plain");
-            intent2.putExtra(Intent.EXTRA_TEXT, "Your text here" );
+            intent2.putExtra(Intent.EXTRA_TEXT, "I'm on the grand tour of the Dulwich outdoor gallery" );
             startActivity(Intent.createChooser(intent2, "Share via"));
+        }
+
+        void navigate(){
+            LatLng last = Core.getGallery().get(lastVisited).getLocation();
+            LatLng next = Core.getGallery().get(lastVisited+1).getLocation();
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+            Uri.parse("http://maps.google.com/maps?saddr="+ last.latitude+ "," + last.longitude+"&daddr="+ next.latitude+ "," + next.longitude));
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/team3m/dulwichoutdoorgallery/RouteProgressIndicator.java
+++ app/src/main/java/team3m/dulwichoutdoorgallery/RouteProgressIndicator.java
@@ -23,16 +23,16 @@ public class RouteProgressIndicator extends View{
         super.onDraw(canvas);
 
         paint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        paint.setStrokeWidth(1.5f);
+        paint.setStrokeWidth(3.5f);
 
         //int half = Core.getGallery().size()/2;
         float radius = getWidth() / Core.getGallery().size();
         //float padding = (getWidth() - Core.getGallery().size() * radius * 2) / (Core.getGallery().size()+1);
-        for(int x =0; x < Core.getGallery().size();x++){
+        for(int x = 0; x < Core.getGallery().size();x++){
             paint.setColor(x == selectedIndex ? getResources().getColor(R.color.brand) : Color.LTGRAY);
             //canvas.drawCircle(x * 10 + (x+1) * padding, getHeight() / 4f * (float)(Math.floor(x/half) + 1f) - radius , radius , paint);
             paint.setStyle(Paint.Style.FILL);
-            canvas.drawRect(x* radius , 0 , x*radius + radius , getHeight(),paint);
+            canvas.drawRect(x* radius  , 0 , x*radius + radius , getHeight(),paint);
 
             paint.setStyle(Paint.Style.STROKE);
             paint.setColor( Color.WHITE);
