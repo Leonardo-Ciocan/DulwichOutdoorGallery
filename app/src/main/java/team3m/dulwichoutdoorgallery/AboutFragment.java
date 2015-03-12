f1e9e31a39cbc28c7ebf83d80a6c270ce896921b
diana-ghm <diana.ghitun@kcl.ac.uk>
2015-03-12 14:24:55 +0000

    Information added in about page

--- app/src/main/java/team3m/dulwichoutdoorgallery/AboutFragment.java
+++ app/src/main/java/team3m/dulwichoutdoorgallery/AboutFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
 
@@ -15,6 +16,11 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView aboutText = (TextView) rootView.findViewById(R.id.aboutText);
+        aboutText.setText("England's oldest public art gallery has been an inspiration for its newest. Dulwich Outdoor Gallery, emerging in 2012, is the collection of art works on the streets in the vicinity of Dulwich Picture " +
+                "Gallery, painted by some of the top street artists alive today whose works have a common theme - they are based on the 17th and 18th " +
+                "century paintings in the permanent collection at Dulwich Picture Gallery.");
         return rootView;
     }
 }
